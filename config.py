fb450a0b293dfe019b1f527ac12f16c0597ae22e
artemyev <147313929+SpillerAtico@users.noreply.github.com>
2023-12-01 03:50:42 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+from view.text_collection import option
+
+attachments = "C:/Users/admin/Desktop/спидозные козявки.xlsx"
+your_option = int(input(option))
---
+++ main.py
@@ -0,0 +1,10 @@
+from general_provisions.table import func_table
+from config import your_option
+
+if __name__ == '__main__':
+    # func_table.get_table_1(your_option)
+    # func_table.get_table_2(your_option)
+    # func_table.get_table_3(your_option)
+    func_table.get_table_4(your_option)
+
+
