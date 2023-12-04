ddde89a460667a19b5a854eba39f40a9bce6d69a
Alexander <uretardfromkld@gmail.com>
2023-12-05 00:06:32 +0300

    complite work 1,2,3

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-from view.text_collection import option
+from view.text_collection import option, work_number
 
 attachments = "C:/Users/admin/Desktop/спидозные козявки.xlsx"
 your_option = int(input(option))
+work_number = int(input(work_number))
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from general_provisions.table import func_table
-from config import your_option
+from config import your_option, work_number
 
 if __name__ == '__main__':
-    # func_table.get_table_1(your_option)
-    # func_table.get_table_2(your_option)
-    # func_table.get_table_3(your_option)
-    # func_table.get_table_4(your_option, action=input('Выводим таблицу с вычислениями? (да/нет): '))
-    # func_table.get_carrying_capacity(your_option, action=input('Выводим таблицу с вычислениями? (да/нет): '))
-    # func_table.get_table_5(your_option)
-    func_table.get_structure_turn(your_option)
+    if work_number == 1:
+        func_table.get_table_1(your_option)
+        func_table.get_table_2(your_option)
+        func_table.get_table_3(your_option)
+
+    if work_number == 2:
+        action = input('Выводим таблицы с вычислениями? (да/нет): ')
+        func_table.get_table_4(your_option, action)
+        func_table.get_carrying_capacity(your_option, action)
+
+    if work_number == 3:
+        action = input('Выводим таблицы с вычислениями? (да/нет): ')
+        func_table.get_table_5(your_option)
+        func_table.get_structure_turn(your_option, action)
